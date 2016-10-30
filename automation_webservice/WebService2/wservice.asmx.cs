3a03e25ff6bdfb4efeac9e1d80c1a5c9b1144d3b
Carlos Sales <causalvador@hotmail.com>
2016-10-30 18:21:47 -0300

    Web-service retorna Json com lista de entregas

--- automation_webservice/WebService2/wservice.asmx.cs
+++ automation_webservice/WebService2/wservice.asmx.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
 using System.Web.Services;
 
 namespace WebService2
@@ -32,7 +32,7 @@ namespace WebService2
             {
                 OperacaoBanco operacao = new OperacaoBanco();
                 Boolean inserir = operacao.Insert(@"insert into Tbl_Historico (ID_Motoboy, Id_Entrega, Data_Coleta, Latitude, Longitude)
-	                                                values (" + IdMotoboy + ", " + IdEntrega + ", GETDATE(), '" + latitude + "','" + longitude + "')");
+	                                                values (" + IdMotoboy + ", " + IdEntrega + ", '" + dataLeitura + "', '" + latitude + "','" + longitude + "')");
                 if (inserir == true)
                 {
                     Resultado = "OK";
@@ -43,6 +43,49 @@ namespace WebService2
                 }
 
                 ConexaoBancoSQL.fecharConexao();
+            }
+            catch (Exception)
+            {
+                Resultado = "FALHA CONEXÃO BANCO DE DADOS";
+            }
+
+            return Resultado;
+        }
+
+        [WebMethod(Description = "Receber Id do Motoboy e retornar JSON com lista de Entregas a realizar.")]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string ListaEntregas(int IdMotoboy)
+        {
+            string Resultado = "";
+            List<Object> resultado = new List<object>();
+            try
+            {
+                OperacaoBanco operacao = new OperacaoBanco();
+                System.Data.SqlClient.SqlDataReader dados = operacao.Select("SELECT Nome_Destinatario,Endereco,Ponto_Ref,Bairro,Cidade,Telefone," 
+                        + "Observacoes,Latitude,Longitude,Id_Motoboy "
+                        + "FROM Tbl_Entregas " 
+                        + "where Entregue<>1 and ID_Motoboy = " + IdMotoboy);
+                while (dados.Read())
+                {
+                    resultado.Add(new
+                    {
+                        Nome = dados[0].ToString(),
+                        Endereco = dados[1].ToString(),
+                        Ponto_Ref = dados[2].ToString(),
+                        Bairro = dados[3].ToString(),
+                        Cidade = dados[4].ToString(),
+                        Telefone = dados[5].ToString(),
+                        Observacoes = dados[6].ToString(),
+                        Latitude = dados[7].ToString(),
+                        Longitude = dados[8].ToString(),
+                    });
+                }
+                ConexaoBancoSQL.fecharConexao();
+
+                //O JavaScriptSerializer vai fazer o web service retornar JSON
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                return js.Serialize(resultado);
+
             }
             catch (Exception)
             {
